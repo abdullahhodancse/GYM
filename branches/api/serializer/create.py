cb0b22ad6928acf44a706f29d70c10909308495b
Abdullah Hodan <abdullahhodan448@gmail.com>
2026-01-14 14:11:29 +0600

    manager email added in branch list

--- branches/api/serializer/create.py
+++ branches/api/serializer/create.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from branches.models.branch import Branch
 
+
 class BranchSerializer(serializers.ModelSerializer):
 
+    manager = serializers.CharField(source="manager.user.email", allow_null=True)  # manager er email ta dekhanor jjonno.
     class Meta:
         model = Branch
-        fields = ["id", "name", "address","phone_number", "created_at"]
+        fields = ["id", "name", "address","phone_number","manager", "created_at"]
         read_only_fields = ["id", "created_at"]
